8e89a7d73256296cef8adaf95aef7ce1ed7b974b
Mohd Najib Bin Ibrahim <mnajib@gmail.com>
2017-05-15 01:22:54 +0800

    Update zon-solat.sh

--- zon-solat.sh
+++ zon-solat.sh
@@ -1,22 +1,46 @@
 #!/bin/bash
 
-fzon1="tmp/zon-waktusolat-page1.html"
-fzon2="tmp/zon-waktusolat-page2.html"
-fzon3="tmp/zon-waktusolat-page3.html"
+#fzon1="tmp/zon-waktusolat-page1.html"
+#fzon2="tmp/zon-waktusolat-page2.html"
+#fzon3="tmp/zon-waktusolat-page3.html"
+fzon4="tmp/zon-waktusolat-page4.html"
 fzon="tmp/zon-waktusolat.csv"
 fzontmp="tmp/zon-waktusolattmp.csv"
 
-curl -s "http://www2.e-solat.gov.my/zon-waktusolat.php" > $fzon1
-curl -s "http://www2.e-solat.gov.my/zon-waktusolat.php?PageNo=2" > $fzon2
-curl -s "http://www2.e-solat.gov.my/zon-waktusolat.php?PageNo=3" > $fzon3
+#curl -s "http://www2.e-solat.gov.my/zon-waktusolat.php" > $fzon1
+#curl -s "http://www2.e-solat.gov.my/zon-waktusolat.php?PageNo=2" > $fzon2
+#curl -s "http://www2.e-solat.gov.my/zon-waktusolat.php?PageNo=3" > $fzon3
+curl -s "http://www2.e-solat.gov.my/zon-waktusolat.php" > $fzon4
+curl -s "http://www2.e-solat.gov.my/zon-waktusolat.php?PageNo=2" >> $fzon4
+curl -s "http://www2.e-solat.gov.my/zon-waktusolat.php?PageNo=3" >> $fzon4
 
-#tr '[A-Z]' '\012' < ussmall
+##tr '[A-Z]' '\012' < ussmall
 
-cat $fzon1 | hxnormalize | hxclean | tr "\n" "\ " | sed 's/\ \ //g' | sed 's/<tr\ /<TR /g' | sed 's/<td\ /<TD /g' |sed 's/<\/tr>/<\/TR>/g' | sed 's/<\/td>/<\/TD>/g' | sed 's/<\/TR>/<\/TR>\n/g' | sed -E 's/<\/TD>/<\/TD>\n/g' | sed 's/<TR\ class.*/<TR>/g' | sed 's/-->/\n/g' | sed 's/^\ //g' | grep '^<TD' | grep -v div | sed -E "s/^<TD\ class='tabletext' vAlign='top' align='left'>&nbsp;//g" | sed -E "s/<\/TD>//g" | sed -E "s/<TD align=\"left\" class=\"tabletext\" valign=\"top\">&nbsp;\ //g" > $fzon
-
-cat $fzon2 | hxnormalize | hxclean | tr "\n" "\ " | sed 's/\ \ //g' | sed 's/<tr\ /<TR /g' | sed 's/<td\ /<TD /g' |sed 's/<\/tr>/<\/TR>/g' | sed 's/<\/td>/<\/TD>/g' | sed 's/<\/TR>/<\/TR>\n/g' | sed -E 's/<\/TD>/<\/TD>\n/g' | sed 's/<TR\ class.*/<TR>/g' | sed 's/-->/\n/g' | sed 's/^\ //g' | grep '^<TD' | grep -v div | sed -E "s/^<TD\ class='tabletext' vAlign='top' align='left'>&nbsp;//g" | sed -E "s/<\/TD>//g" | sed -E "s/<TD align=\"left\" class=\"tabletext\" valign=\"top\">&nbsp;\ //g" >> $fzon
-
-cat $fzon3 | hxnormalize | hxclean | tr "\n" "\ " | sed 's/\ \ //g' | sed 's/<tr\ /<TR /g' | sed 's/<td\ /<TD /g' |sed 's/<\/tr>/<\/TR>/g' | sed 's/<\/td>/<\/TD>/g' | sed 's/<\/TR>/<\/TR>\n/g' | sed -E 's/<\/TD>/<\/TD>\n/g' | sed 's/<TR\ class.*/<TR>/g' | sed 's/-->/\n/g' | sed 's/^\ //g' | grep '^<TD' | grep -v div | sed -E "s/^<TD\ class='tabletext' vAlign='top' align='left'>&nbsp;//g" | sed -E "s/<\/TD>//g" | sed -E "s/<TD align=\"left\" class=\"tabletext\" valign=\"top\">&nbsp;\ //g" >> $fzon
+cat $fzon4 | \
+	hxnormalize | hxclean | \
+	tr "\n" "\ " | \
+	sed 's/\ \ //g' | \
+	sed 's/<tr\ /<TR /g' | \
+	sed 's/<td\ /<TD /g' | \
+	sed 's/<\/tr>/<\/TR>/g' | \
+	sed 's/<\/td>/<\/TD>/g' | \
+	sed 's/<\/TR>/<\/TR>\n/g' | \
+	sed -E 's/<\/TD>/<\/TD>\n/g' | \
+	sed 's/<TR\ class.*/<TR>/g' | \
+	sed 's/<!---able to update solution only--->/\n/g' | \
+	sed 's/^\ //g' | \
+	sed -E 's/<TR\ bgcolor=\"#.{6}\">/<TR>\n/g' | \
+	grep '^<TD' | \
+	grep 'tabletext' | \
+	sed -E "s/<TD\ class='tabletext' vAlign='top' align='left'>//g" | sed -E "s/<\/TD>//g" | \
+	sed -E "s/<TD align=\"left\" class=\"tabletext\" valign=\"top\">//g" | \
+	sed -E "s/&nbsp;//g" | \
+	sed -E "s/<div align=\"left\"><b>//g" | sed -E "s/<\/b><\/div>//g" | \
+	sed -E "s/<TD class='tabletext' vAlign=top align=left>//g" | \
+	sed -E "s/^\ *//g" | \
+	sed -E "s/,\ */, /g" \
+	> $fzon
+#cat $fzon
 
 cat $fzon > $fzontmp
 ed - $fzontmp << EOF
@@ -26,7 +50,14 @@ ed - $fzontmp << EOF
 w
 q
 EOF
+#cat $fzontmp
 
-while read line1; do read line2; echo "${line2}, ${line1}"; done < $fzontmp > $fzon
-
+#cat $fzon > $fzontmp
+while read line1; do 
+	read line2
+	read line3
+	read line4
+	#echo "${line1}, ${line4}, ${line2}, ${line3}" 
+	echo "${line4}, ${line2}, ${line3}" 
+done < $fzontmp > $fzon
 cat $fzon
