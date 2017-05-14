b20055b86aa3744e8d5b794b28dc21c03ad1f716
Mohd Najib Bin Ibrahim <mnajib@gmail.com>
2017-05-15 01:59:24 +0800

    Update waktu-solat.sh

--- waktu-solat.sh
+++ waktu-solat.sh
@@ -10,7 +10,6 @@
 #    ~/bin/waktu-solat.sh SGR04
 #
 # Requirement:
-#    bash
 #    xmlstarlet
 #
 # Install:
@@ -23,6 +22,12 @@
 #    SGR04 Putrajaya
 #
 
+#file='/tmp/waktu-solat-najib.xml'
+#file="/tmp/waktu-solat-${USER}.xml"
+file="tmp/waktu-solat.xml"
+getzon="bin/zon-solat.sh"
+zonelist="tmp/zonelist.csv"
+
 defaultZone="SGR04"
 if [ "$1" == "" ]; then
   zone=$defaultZone
@@ -30,16 +35,36 @@ else
   zone=$1
 fi
 
-file="/tmp/waktu-solat-${USER}.xml"
+# get list of zon solat
+bash $getzon > $zonelist
 
-curl -s "http://www2.e-solat.gov.my/xml/today/?zon=${zone}" > $file
+# grep/awk/sed zone code
+#zonecode=$zone
+zonecode=`cat $zonelist | grep -i "$zone" | awk '{ print $1 }'| sed 's/"//g' | sed 's/,//g' | head -1`
+echo "zonecode=$zonecode"
+
+# if get zone code failed
+# print error and help/usage
+
+#curl -s "http://www2.e-solat.gov.my/xml/today/?zon=${zone}" > $file
+curl -s "http://www2.e-solat.gov.my/xml/today/?zon=${zonecode}" > $file
 echo "======================================"
 
-location=`cat $file | xmlstarlet sel -T -t -m /rss/channel -v "description" -n | sed 's/\ *//g' | sed 's/,/, /g'`
-echo "Location: $zone ($location)"
+#cat $file | xmlstarlet sel -T -t -m /rss/channel -v "concat('Location: $zone (', description, ')')" -n
 
+# Print zone
+#location=`cat $file | xmlstarlet sel -T -t -m /rss/channel -v "description" -n | sed 's/\ *//g' | sed 's/,/, /g'`
+#location=`cat $file | xmlstarlet sel -T -t -m /rss/channel -v "description" -n`
+location=`cat $file | xmlstarlet sel -T -t -m /rss/channel -v "description" -n | sed 's/\ \ *//g' | sed 's/,/, /g' | sed 's/\ *$//g'`
+#echo "Location: $zone ($location)"
+echo "Location: $zonecode ($location)"
+
+# Print current data and time
 cat $file | xmlstarlet sel -T -t -m /rss/channel -v "concat('Current date/time: ', dc:date)" -n
+
 echo "--------------------------------------"
 
+# ...
 cat $file | xmlstarlet sel -T -t -m /rss/channel/item -v "concat(title, ' ', description)" -n
+
 echo "======================================"
